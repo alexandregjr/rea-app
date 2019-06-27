17dd637944b9bc7a5cab3f0470672607e2c96137
alexandregjr <alexandregalochajr@gmail.com>
2019-06-27 15:16:01 -0300

    Fixed route bug

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Subject from './Subject'
+import contentData from '../contentData'
 
 function Content(props) {
         let { id } = props.match.params
+
         if (!id) id = 1
+        if (isNaN(parseInt(id))) id = 1
+        if (!isNaN(parseInt(id))) id = parseInt(id)
+        if (id > contentData.length) id = 1
 
         return(
             <main>
