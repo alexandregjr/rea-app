f819b1ce5804a81e76bc26cf50cff88b93894be1
Tiago <tmsilva11@usp.br>
2021-11-23 17:46:38 -0300

    fix: keyboard navigation with tab
    co-authored-by: Alex <alexgalocha.jr@gmail.com>

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -6,14 +6,14 @@ function Subject(props) {
     const data = contentData[(props.from)];
     const subtopics = data.subtopics.map((topic, index) => (
         <div className={'subtopic'} key={index}>
-            <h3>{topic.title}</h3>
+            <h3 id={topic.title}><a href={`#${topic.title}`}>{topic.title}</a></h3>
             {topic.content}
         </div>
     ))
     
     return (
         <div>
-            <h2>{data.title}</h2>
+            <h2 id={data.title}><a href={`#${data.title}`}>{data.title}</a></h2>
             {subtopics}
             <Exercise subjectId={(props.from)} />
         </div>
