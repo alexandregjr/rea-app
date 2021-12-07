0ce6c55f7c4b4ca94cd7acd0c7c03e8a7d240800
LauraGenari <LauraGenari@icarotech.com>
2021-12-06 23:10:40 -0300

    ta saindo do forninho

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 import contentData from '../contentData'
+import menu from '../menu.png'
+import Menu from './Menu';
 
 function Header(props) {
     const titles = contentData.map((topic) => (
         {title: topic.title}
     ))
-
+    const [click, setClick] = useState(false);
     return(
         <header>
             <div className={'content'}>
-                <h1>{Array.from("AGPJEPLGAJTMS").sort().join('')} <span >Estudos</span></h1>
-                <Navbar topics={titles}/>
+                <div style={{display:"inline-flex"}}>
+                    <img src={menu} style={{zIndex:'1', height: "3rem", width:"3rem", paddingTop:"0.75rem" }} onClick={()=>{setClick(!click)}} />
+                    <Menu click={click}/>
+                    <h1>{Array.from("AGPJEPLGAJTMS").sort().join('')} <span>Estudos</span></h1>
+                </div>
+                <Navbar topics={titles} click={click} />
             </div>
         </header>
     )
---
+++ src/components/Menu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Menu ({click}) {
+
+  return <div style={{position: "absolute", height: "100vh", width:"20vw", backgroundColor:"#34495e", display: click ? "block" : "none", margin:'0'}}>
+
+  </div>
+
+}
